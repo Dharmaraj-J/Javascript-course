a9fe24e769074c1539e811cf7cebf6dce0bacc95
Dharmarj-J <dharmaraj200303@gmail.com>
2023-07-09 10:38:35 -1200

    updated

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -10,24 +10,35 @@ const todoList = [
 ];
 
 renderTodoList();
+
 function renderTodoList() {
   let todoListHtml = "";
 
-  todoList.forEach(function (todoObject, index) {
+  todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
     const html = `
     <div>${name}</div>
     <div>${dueDate}</div>
-    <button onclick="
-      todoList.splice(${index}, 1);
-      renderTodoList();
-    "class="delete-todo-button">Delete</button>
+    <button class="delete-todo-button js-delete-todo-button">Delete</button>
     `;
     todoListHtml += html;
   });
 
   document.querySelector(".js-todo-list").innerHTML = todoListHtml;
+
+  document
+    .querySelectorAll(".js-delete-todo-button")
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener("click", () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    });
 }
+
+document.querySelector(".js-add-todo-button").addEventListener("click", () => {
+  addTodo();
+});
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
